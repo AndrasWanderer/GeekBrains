43d1c2d61309755bf24f246c0c32039035180a43
Andras <warlock61636@yandex.ru>
2023-06-23 00:37:55 +0300

    Решена вторая задача ДЗ

---
+++ Seminar1/SeminarHomework/HomeworkTask2/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int a,b,c;
+
+Console.Write("Введите первое число: ");
+
+a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+
+b = int.Parse(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+
+c = int.Parse(Console.ReadLine());
+
+
+if(a>b)
+{
+    if(a>c)
+    { 
+        Console.WriteLine($"Максимальное число = {a}");
+    }
+    else
+    {
+        Console.WriteLine($"Максимальное число = {c}");
+    }
+}     
+else if(b>c)
+{
+    Console.WriteLine($"Максимальное число = {b}");
+}
+else
+{
+    Console.WriteLine($"Максимальное число = {c}");
+}
