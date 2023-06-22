2310521d87f30160a22f3856e41b35225d32b681
Andras <warlock61636@yandex.ru>
2023-06-23 00:01:34 +0300

    Решена первая задача ДЗ

---
+++ Seminar1/SeminarHomework/HomeworkTask1/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 2: 
+//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+
+Console.Write("Введите первое число: ");
+
+int a,b;
+
+a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+
+b = int.Parse(Console.ReadLine());
+
+if(a>b)
+{
+   Console.WriteLine($"{a} больше, чем {b}");
+} else if (a<b)
+    {
+         Console.WriteLine($"{b} больше, чем {a}");
+}
+else
+{
+    Console.WriteLine("Числа равны");
+}
