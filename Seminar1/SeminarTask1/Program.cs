523e915d7e0f5383a90f2f5f3301135f53e48587
Andras <warlock61636@yandex.ru>
2023-06-22 22:48:04 +0300

    Задачи которые были на уроке кроме двух последних

---
+++ Seminar1/SeminarTask1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+
+Console.Write("Enter the numberA:");
+
+int numberA = int.Parse(Console.ReadLine());
+
+Console.Write("Enter the numberB:");
+
+int numberB = int.Parse(Console.ReadLine());
+
+if(numberB*numberB == numberA)
+{
+    Console.WriteLine("Да");
+}
+
+else
+{
+    Console.WriteLine("Нет");
+}
---
+++ Seminar1/SeminarTask3/Program.cs
@@ -0,0 +1,4 @@
+﻿ //Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+Console.WriteLine("Enter tne number: ");
+int number = int.Parse(Console.ReadLine());
