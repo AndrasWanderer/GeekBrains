b29d968a10b4de48cd20c9065be0ff6063461578
Andras <warlock61636@yandex.ru>
2023-07-03 20:48:56 +0300

    Новый урок

---
+++ Seminar4/SeminarTask1/Program.cs
@@ -0,0 +1,26 @@
+﻿//задача про палиндром
+
+bool Ispalindrome(int number)
+{
+    bool result = false;
+  
+    int revNumber = 0;
+    int tempSourceNumber = number;
+    while(tempSourceNumber != 0)
+    {
+        int temp = number % 10;
+
+        revNumber = (revNumber * 10) + temp;
+
+        number = number / 10;
+    }
+
+    if(revNumber == number)
+    {
+        result = true;
+    }
+
+    return result;
+}
+
+Console.WriteLine(Ispalindrome(13431));
