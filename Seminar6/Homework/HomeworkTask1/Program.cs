cb5749576224336440082e459ee88a33c6109ce7
Andras <warlock61636@yandex.ru>
2023-07-12 21:35:51 +0300

    Homework tasks complete

--- Seminar6/Homework/HomeworkTask1/Program.cs
+++ Seminar6/Homework/HomeworkTask1/Program.cs
@@ -1,2 +1,19 @@
 ﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. 
 //Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите количество M: ");
+int m = int.Parse(Console.ReadLine());
+
+int count = 0;
+for (int i = 1; i <= m; i++)
+{
+    Console.Write($"Введите {i}-ое число: ");
+    int number = int.Parse(Console.ReadLine());
+
+    if (number > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Количество чисел, больших 0: {count}");
--- Seminar6/Homework/HomeworkTask2/Program.cs
+++ Seminar6/Homework/HomeworkTask2/Program.cs
@@ -1,3 +1,15 @@
 ﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
 //заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
 //значения b1, k1, b2 и k2 задаются пользователем.
+
+
+Console.WriteLine("Введите значения b1, k1, b2 и k2:");
+        double b1 = double.Parse(Console.ReadLine());
+        double k1 = double.Parse(Console.ReadLine());
+        double b2 = double.Parse(Console.ReadLine());
+        double k2 = double.Parse(Console.ReadLine());
+
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+
+        Console.WriteLine("Точка пересечения: ({0}, {1})", x, y);
