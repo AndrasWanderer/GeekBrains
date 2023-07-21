bc61ec3a12cb2d55d4d14c067c66746ff55e25d8
Andras <warlock61636@yandex.ru>
2023-07-21 17:32:35 +0300

    Seminar9 HW Complete

---
+++ Seminar9/HomeWork/HomeworkTask1/Program.cs
@@ -0,0 +1,37 @@
+﻿/* Дан одномерный массив целых чисел, элементом которого, может быть либо 1, либо 0.
+Ваша задача вывести на экран максимальное количество следующих друг за другом 1.
+
+Пример 1:
+Исходный массив: [1, 1, 0, 1, 1, 1]
+*/
+
+
+int[] array = new int[10];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(2);
+}
+
+Console.WriteLine("Исходный массив: " + string.Join(", ", array));
+
+int maxCount = 0;
+int currentCount = 0;
+
+foreach (int element in array)
+{
+    if (element == 1)
+    {
+        currentCount++;
+        if (currentCount > maxCount)
+        {
+            maxCount = currentCount;
+        }
+    }
+    else
+    {
+        currentCount = 0;
+    }
+}
+
+Console.WriteLine("Максимальное количество последовательных единиц: " + maxCount);
---
+++ Seminar9/HomeWork/HomeworkTask3/Program.cs
@@ -0,0 +1,65 @@
+﻿/*
+Дана строка, содержащая только следующие символы:'(', ')', '{', '}', '[', ']'. Определите, является ли ваша строка допустимой.
+Исходная строка считается допустимой при условии:
+1. Открытые скобки должны быть закрыты скобками того же типа.
+2. Открытые скобки должны быть закрыты в правильном порядке.
+4. Каждая закрывающия скобка имеет соответствующую открытыю скобку того же типа.
+
+Пример 1:
+Входная строка: s = "()"
+Результат: true
+
+Пример 2:
+Входная строка: s = "()[]{}"
+Результат: true
+
+Пример 3:
+Входная строка: s = "(]"
+Результат: false
+
+Пример 4:
+Входная строка: s = "()[]{()}"
+Результат: true
+*/
+
+static bool IsValidString(string s)
+{
+    Dictionary<char, char> bracketsMap = new Dictionary<char, char>
+        {
+            { ')', '(' },
+            { '}', '{' },
+            { ']', '[' }
+        };
+
+    List<char> openBrackets = new List<char>();
+
+    foreach (char c in s)
+    {
+        if (c == '(' || c == '{' || c == '[')
+        {
+            openBrackets.Add(c);
+        }
+        else if (c == ')' || c == '}' || c == ']')
+        {
+            if (openBrackets.Count == 0)
+            {
+                return false;
+            }
+
+            char top = openBrackets[openBrackets.Count - 1];
+            openBrackets.RemoveAt(openBrackets.Count - 1);
+
+            if (top != bracketsMap[c])
+            {
+                return false; 
+            }
+        }
+    }
+
+    return openBrackets.Count == 0;
+}
+//---------------------------------------------------------------------
+Console.Write("Введите знаки: ");
+string s = Console.ReadLine();
+bool result = IsValidString(s);
+Console.WriteLine(result);
